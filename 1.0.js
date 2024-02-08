1205f360d46af94b4cf5f75d2c074019cdb875b5
Luke Park <143543147+lukep258@users.noreply.github.com>
2024-02-07 22:28:58 -0600

    lesson 1

---
+++ 1.0.js
@@ -0,0 +1,69 @@
+var studentList = [
+  {
+    name: "Victor",
+    age: 15,
+    grade: 95,
+    classList: ["algebra", "english", "spanish", "social studies", "physics"],
+  },
+  {
+    name: "Karen",
+    age: 16,
+    grade: 82,
+    classList: [
+      "geometry",
+      "creative writing",
+      "italian",
+      "US history",
+      "biology",
+    ],
+  },
+  {
+    name: "Reed",
+    age: 18,
+    grade: 88,
+    classList: [
+      "calculus",
+      "technical writing",
+      "spanish",
+      "european history",
+      "chemistry",
+    ],
+  },
+  {
+    name: "Fiona",
+    age: 18,
+    grade: 92,
+    classList: [
+      "calculus",
+      "business writing",
+      "chinese",
+      "european history",
+      "chemistry",
+    ],
+  },
+  {
+    name: "Damon",
+    age: 17,
+    grade: 75,
+    classList: [
+      "trigonometry",
+      "creative writing",
+      "french",
+      "US history",
+      "biology",
+    ],
+  },
+];
+
+// 1. console log just technical writing
+// 2. console log Damon's grade
+// 3. pop away chemistry from Fiona's classList and push AP chemistry into it instead
+// 4. change Karen's age to 17
+// 5. write a function that takes at least 1 integer as a parameter. The function will change the grade of one of the students and return that student's grade
+
+// 6. write a movieList array that contains 5 objects. Each object will have they keys: title, release date, and director.
+// 6a. first movie: totoro (1988) - Hayao Miyazaki
+// 6b. second movie: bullet train (2022) - David Leitch
+// 6c. third movie: home alone (1990) - Chris Columbus
+// 6d. fourth movie: fantastic four (2015) - Josh Trank
+// 6e. fifth movie: the incredibles (2004) - Brad Bird
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# JS_Practice
+1. Core JS
+2. Core HTML/CSS
+3. DOM manipulation & event handling
+4. API calls
+5. React with prop drilling
+6. React with context API
+7. React with react router
